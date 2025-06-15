49e5727b634a9de28ae3f5e9b0cda912c8d2f7df
Johannes Brown Porsgaard <jbp@secomea.com>
2025-06-15 15:14:12 +0200

    feat: move core engine to separate file, and begin implementing Holdem

---
+++ src/core_engine.rs
@@ -0,0 +1,239 @@
+use std::{cmp::Ordering, collections::HashMap};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Suit {
+    Hearts,
+    Spades,
+    Diamonds,
+    Clubs,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Card {
+    pub suit: Suit,
+    pub value: u8,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+enum Ranking {
+    HighCard,
+    Pair(u8),
+    TwoPairs(u8, u8),
+    ThreeOfAKind(u8),
+    Straight(u8),
+    Flush,
+    FullHouse(u8, u8),
+    FourOfAKind(u8),
+    StraightFlush(u8),
+}
+
+use Ranking::*;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Hand(pub [Card; 5]);
+
+impl Hand {
+    fn as_sorted(&self) -> Hand {
+        let mut cards = self.0.clone();
+        cards.sort_by_key(|card| card.value);
+        cards.reverse();
+        Hand(cards)
+    }
+
+    fn as_values(&self) -> [u8; 5] {
+        self.as_sorted().0.clone().map(|card| card.value)
+    }
+
+    fn get_values_with_ace_as_one(&self) -> [u8; 5] {
+        let mut values = self.as_values();
+
+        for val in &mut values {
+            if *val == 14 {
+                *val = 1;
+            }
+        }
+        values.sort();
+        values.reverse();
+        values
+    }
+
+    fn get_ranking(&self) -> Ranking {
+        return self
+            .try_get_flush()
+            .or_else(|| self.try_get_straight())
+            .or_else(|| self.try_get_group_based_ranking())
+            .unwrap_or(HighCard);
+    }
+
+    fn try_get_flush(&self) -> Option<Ranking> {
+        let suit = &self.0[0].suit;
+        if self.0.iter().all(|card| card.suit == *suit) {
+            if let Some(Straight(s)) = self.try_get_straight() {
+                Some(StraightFlush(s))
+            } else {
+                Some(Flush)
+            }
+        } else {
+            None
+        }
+    }
+
+    fn try_get_straight(&self) -> Option<Ranking> {
+        fn find_straight(values: [u8; 5]) -> Option<u8> {
+            if values.windows(2).all(|w| w[0] == w[1] + 1) {
+                Some(values[0])
+            } else {
+                None
+            }
+        }
+
+        find_straight(self.as_values())
+            .or_else(|| find_straight(self.get_values_with_ace_as_one()))
+            .map(Straight)
+    }
+
+    fn try_get_group_based_ranking(&self) -> Option<Ranking> {
+        match self.get_groups()[..] {
+            [(v, 4)] => Some(FourOfAKind(v)),
+            [(v1, 3), (v2, 2)] => Some(FullHouse(v1, v2)),
+            [(v, 3)] => Some(ThreeOfAKind(v)),
+            [(v1, 2), (v2, 2)] => Some(TwoPairs(v1, v2)),
+            [(v, 2)] => Some(Pair(v)),
+            _ => None,
+        }
+    }
+
+    fn get_groups(&self) -> Vec<(u8, usize)> {
+        let mut groups: HashMap<u8, usize> = HashMap::new();
+
+        let values = self.as_values();
+        let mut last_val = values[0];
+
+        for value in values[1..].iter() {
+            if *value == last_val {
+                *groups.entry(*value).or_insert(1) += 1;
+            }
+            last_val = *value;
+        }
+
+        let mut groups: Vec<(u8, usize)> = groups.iter().map(|(&k, &v)| (k, v)).collect();
+        groups.sort_by(|a, b| b.1.cmp(&a.1).then_with(|| b.0.cmp(&a.0)));
+        groups
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let self_ranking = self.get_ranking();
+        let other_ranking = other.get_ranking();
+        let ranking_comp = self_ranking.cmp(&other_ranking);
+
+        if ranking_comp != Ordering::Equal {
+            return ranking_comp;
+        }
+
+        self.as_values().cmp(&other.as_values())
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Hand {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl Eq for Hand {}
+
+#[cfg(test)]
+mod tests {
+    use super::Suit::*;
+    use super::*;
+
+    #[test]
+    fn test_hands_with_same_value_are_equal() {
+        assert_hands_are_equal("H4 D4 D13 C12 H10", "D10 C13 C4 H12 S4");
+    }
+
+    #[test]
+    fn test_all_pairs_of_hands() {
+        let hands = [
+            "D3 D6 S12 H9 C4",
+            "D3 D10 S13 H9 C2",
+            "H2 H5 S10 C9 D13",
+            "D3 D9 C9 S4 H13",
+            "H3 C4 H10 D10 D9",
+            "H2 H5 S10 C10 S9",
+            "D6 H6 H3 S3 H10",
+            "H2 C2 C7 S7 H9",
+            "H3 H7 S8 S3 C3",
+            "C14 C2 D3 D4 S5",
+            "H4 S5 C8 H7 H6",
+            "D5 D8 D2 D12 D9",
+            "S5 C5 H5 S8 C8",
+            "S5 C5 H8 S8 C8",
+            "H4 S4 C4 D4 S9",
+            "H14 H2 H3 H4 H5",
+            "C14 C13 C12 C11 C10",
+        ];
+
+        for (i, hand) in hands.iter().enumerate() {
+            for lesser_hand in hands[..i].iter() {
+                assert_first_hand_wins(hand, lesser_hand);
+            }
+            assert_hands_are_equal(hand, hand);
+        }
+    }
+
+    fn assert_hands_are_equal(first: &str, second: &str) {
+        let comp = create_hand(first).cmp(&create_hand(second));
+        assert_eq!(comp, Ordering::Equal);
+        assert_eq!(create_hand(first), create_hand(second));
+    }
+
+    fn assert_first_hand_wins(first: &str, second: &str) {
+        let first_hand = create_hand(first);
+        let second_hand = create_hand(second);
+
+        assert!(
+            first_hand > second_hand,
+            "Hand >>{first}<< should win over >>{second}<< but didn't"
+        );
+        assert!(
+            second_hand < first_hand,
+            "Hand >>{first}<< didn't win over >>{second}<< in the second try"
+        );
+    }
+
+    fn create_hand(hand_as_str: &str) -> Hand {
+        let mut x = hand_as_str.split(' ').map(create_card);
+        Hand([
+            x.next().expect("Not enough cards"),
+            x.next().expect("Not enough cards"),
+            x.next().expect("Not enough cards"),
+            x.next().expect("Not enough cards"),
+            x.next().expect("Not enough cards"),
+        ])
+    }
+
+    fn create_card(card_as_str: &str) -> Card {
+        let suit = card_as_str.chars().next();
+
+        let suit = match suit.expect("can't make a card from an empty string") {
+            'H' => Hearts,
+            'S' => Spades,
+            'D' => Diamonds,
+            'C' => Clubs,
+            _ => panic!("Not a valid suit"),
+        };
+
+        let value: u8 = card_as_str[1..].parse().expect("Not a valid number");
+        Card { suit, value }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,243 +1,207 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::collections::HashMap;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod core_engine;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use PokerAction::*;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-enum Suit {
-    Hearts,
-    Spades,
-    Diamonds,
-    Clubs,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct Card {
-    suit: Suit,
-    value: u8,
-}
-
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-enum Ranking {
-    HighCard,
-    Pair(u8),
-    TwoPairs(u8, u8),
-    ThreeOfAKind(u8),
-    Straight(u8),
-    Flush,
-    FullHouse(u8, u8),
-    FourOfAKind(u8),
-    StraightFlush(u8),
-}
-
-use Ranking::*;
-
-#[allow(dead_code)]
-#[derive(Debug)]
-struct Hand([Card; 5]);
-
-impl Hand {
-    fn as_sorted(&self) -> Hand {
-        let mut cards = self.0.clone();
-        cards.sort_by_key(|card| card.value);
-        cards.reverse();
-        Hand(cards)
+    #[test]
+    fn should_not_be_able_to_play_with_less_than_2_players() {
+        assert!(GameState::init(0).is_none());
+        assert!(GameState::init(1).is_none());
     }
 
-    fn as_values(&self) -> [u8; 5] {
-        self.as_sorted().0.clone().map(|card| card.value)
+    #[test]
+    fn should_win_blind_when_other_player_folds() {
+        let mut sut = TwoPlayerGameTestContainer::init();
+
+        sut.then_player_has_chips(0, 99);
+        sut.then_player_has_chips(1, 98);
+
+        sut.when_player_plays(0, PokerAction::Fold);
+
+        sut.then_player_has_chips(0, 99);
+        sut.then_player_has_chips(1, 101);
     }
 
-    fn get_values_with_ace_as_one(&self) -> [u8; 5] {
-        let mut values = self.as_values();
+    struct TwoPlayerGameTestContainer {
+        gs: GameState,
+    }
 
-        for val in &mut values {
-            if *val == 14 {
-                *val = 1;
+    impl TwoPlayerGameTestContainer {
+        fn init() -> TwoPlayerGameTestContainer {
+            TwoPlayerGameTestContainer {
+                gs: GameState::init(2).unwrap(),
             }
         }
-        values.sort();
-        values.reverse();
-        values
-    }
 
-    fn get_ranking(&self) -> Ranking {
-        return self
-            .try_get_flush()
-            .or_else(|| self.try_get_straight())
-            .or_else(|| self.try_get_group_based_ranking())
-            .unwrap_or(HighCard);
-    }
+        fn then_player_has_chips(&self, player: usize, expected_chips: u32) {
+            assert_eq!(self.gs.current_chips(player), expected_chips);
+        }
 
-    fn try_get_flush(&self) -> Option<Ranking> {
-        let suit = &self.0[0].suit;
-        if self.0.iter().all(|card| card.suit == *suit) {
-            if let Some(Straight(s)) = self.try_get_straight() {
-                Some(StraightFlush(s))
-            } else {
-                Some(Flush)
+        fn when_player_plays(&mut self, player: u32, action: PokerAction) -> () {
+            self.gs = std::mem::replace(&mut self.gs, Self::empty_game_state()).play_action(action);
+        }
+
+        fn empty_game_state() -> GameState {
+            GameState {
+                player_chips: vec![],
             }
+        }
+    }
+}
+
+struct GameState {
+    player_chips: Vec<u32>,
+}
+
+impl GameState {
+    fn init(players: usize) -> Option<Self> {
+        if players > 1 {
+            Some(GameState {
+                player_chips: vec![99, 98],
+            })
         } else {
             None
         }
     }
 
-    fn try_get_straight(&self) -> Option<Ranking> {
-        fn find_straight(values: [u8; 5]) -> Option<u8> {
-            if values.windows(2).all(|w| w[0] == w[1] + 1) {
-                Some(values[0])
-            } else {
-                None
-            }
-        }
-
-        find_straight(self.as_values())
-            .or_else(|| find_straight(self.get_values_with_ace_as_one()))
-            .map(Straight)
+    fn current_chips(&self, player: usize) -> u32 {
+        self.player_chips[player]
     }
 
-    fn try_get_group_based_ranking(&self) -> Option<Ranking> {
-        match self.get_groups()[..] {
-            [(v, 4)] => Some(FourOfAKind(v)),
-            [(v1, 3), (v2, 2)] => Some(FullHouse(v1, v2)),
-            [(v, 3)] => Some(ThreeOfAKind(v)),
-            [(v1, 2), (v2, 2)] => Some(TwoPairs(v1, v2)),
-            [(v, 2)] => Some(Pair(v)),
-            _ => None,
+    fn play_action(self, action: PokerAction) -> Self {
+        let mut player_chips = self.player_chips;
+        player_chips[1] = 101;
+        GameState { player_chips }
+    }
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+struct Game {
+    player: u32,
+    chips: HashMap<u32, u32>,
+    pot: u32,
+}
+impl Game {
+    fn new() -> Self {
+        Self {
+            player: 0,
+            chips: HashMap::new(),
+            pot: 0,
         }
     }
 
-    fn get_groups(&self) -> Vec<(u8, usize)> {
-        let mut groups: HashMap<u8, usize> = HashMap::new();
+    fn register_player(self, player: &impl Player) -> (Self, u32) {
+        let mut chips = self.chips;
+        chips.insert(self.player, 100);
+        (
+            Self {
+                player: self.player + 1,
+                chips,
+                ..self
+            },
+            self.player,
+        )
+    }
 
-        let values = self.as_values();
-        let mut last_val = values[0];
+    fn play_step(self) -> Self {
+        self.put_chips_in_pot(0, 2).put_chips_in_pot(1, 1)
+    }
 
-        for value in values[1..].iter() {
-            if *value == last_val {
-                *groups.entry(*value).or_insert(1) += 1;
-            }
-            last_val = *value;
+    fn put_chips_in_pot(self, player: u32, amount: u32) -> Self {
+        let mut chips = self.chips;
+        chips.insert(player, chips.get(&player).unwrap() - amount);
+        Self {
+            chips,
+            pot: self.pot + amount,
+            ..self
         }
+    }
+    fn current_chips(&self, pid: &u32) -> u32 {
+        *self.chips.get(pid).unwrap()
+    }
 
-        let mut groups: Vec<(u8, usize)> = groups.iter().map(|(&k, &v)| (k, v)).collect();
-        groups.sort_by(|a, b| b.1.cmp(&a.1).then_with(|| b.0.cmp(&a.0)));
-        groups
+    fn play_round(self) -> Self {
+        let mut chips = self.chips;
+        chips.insert(0, chips.get(&0).unwrap() + self.pot);
+        Self {
+            chips,
+            pot: 0,
+            ..self
+        }
     }
 }
 
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let self_ranking = self.get_ranking();
-        let other_ranking = other.get_ranking();
-        let ranking_comp = self_ranking.cmp(&other_ranking);
-
-        if ranking_comp != Ordering::Equal {
-            return ranking_comp;
-        }
-
-        self.as_values().cmp(&other.as_values())
-    }
+pub enum PokerAction {
+    CallOrCheck,
+    Fold,
 }
 
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
+pub trait Player {
+    fn do_play() -> PokerAction;
 }
-
-impl PartialEq for Hand {
-    fn eq(&self, other: &Self) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-
-impl Eq for Hand {}
 
 #[cfg(test)]
-mod tests {
-    use super::Suit::*;
+mod tests2 {
     use super::*;
 
     #[test]
-    fn test_hands_with_same_value_are_equal() {
-        assert_hands_are_equal("H4 D4 D13 C12 H10", "D10 C13 C4 H12 S4");
+    fn should_win_blind_when_other_player_folds() {
+        let player1 = AlwaysCallingPlayer {};
+        let player2 = AlwaysFoldingPlayer {};
+        let (game, player1id) = Game::new().register_player(&player1);
+        let (game, player2id) = game.register_player(&player2);
+
+        assert_ne!(player1id, player2id);
+        assert_eq!(game.current_chips(&player1id), 100);
+        assert_eq!(game.current_chips(&player2id), 100);
+
+        let game = game.play_step();
+
+        assert_eq!(game.current_chips(&player1id), 98);
+        assert_eq!(game.current_chips(&player2id), 99);
+
+        let game = game.play_round();
+
+        assert_eq!(game.current_chips(&player1id), 101);
+        assert_eq!(game.current_chips(&player2id), 99);
     }
 
     #[test]
-    fn test_all_pairs_of_hands() {
-        let hands = [
-            "D3 D6 S12 H9 C4",
-            "D3 D10 S13 H9 C2",
-            "H2 H5 S10 C9 D13",
-            "D3 D9 C9 S4 H13",
-            "H3 C4 H10 D10 D9",
-            "H2 H5 S10 C10 S9",
-            "D6 H6 H3 S3 H10",
-            "H2 C2 C7 S7 H9",
-            "H3 H7 S8 S3 C3",
-            "C14 C2 D3 D4 S5",
-            "H4 S5 C8 H7 H6",
-            "D5 D8 D2 D12 D9",
-            "S5 C5 H5 S8 C8",
-            "S5 C5 H8 S8 C8",
-            "H4 S4 C4 D4 S9",
-            "H14 H2 H3 H4 H5",
-            "C14 C13 C12 C11 C10",
-        ];
+    fn should_not_play_if_less_than_two_players() {}
 
-        for (i, hand) in hands.iter().enumerate() {
-            for lesser_hand in hands[..i].iter() {
-                assert_first_hand_wins(hand, lesser_hand);
-            }
-            assert_hands_are_equal(hand, hand);
+    use PokerAction::*;
+
+    struct AlwaysCallingPlayer {}
+
+    impl Player for AlwaysCallingPlayer {
+        fn do_play() -> PokerAction {
+            CallOrCheck
         }
     }
 
-    fn assert_hands_are_equal(first: &str, second: &str) {
-        let comp = create_hand(first).cmp(&create_hand(second));
-        assert_eq!(comp, Ordering::Equal);
-        assert_eq!(create_hand(first), create_hand(second));
-    }
+    struct AlwaysFoldingPlayer {}
 
-    fn assert_first_hand_wins(first: &str, second: &str) {
-        let first_hand = create_hand(first);
-        let second_hand = create_hand(second);
-
-        assert!(
-            first_hand > second_hand,
-            "Hand >>{first}<< should win over >>{second}<< but didn't"
-        );
-        assert!(
-            second_hand < first_hand,
-            "Hand >>{first}<< didn't win over >>{second}<< in the second try"
-        );
-    }
-
-    fn create_hand(hand_as_str: &str) -> Hand {
-        let mut x = hand_as_str.split(' ').map(create_card);
-        Hand([
-            x.next().expect("Not enough cards"),
-            x.next().expect("Not enough cards"),
-            x.next().expect("Not enough cards"),
-            x.next().expect("Not enough cards"),
-            x.next().expect("Not enough cards"),
-        ])
-    }
-
-    fn create_card(card_as_str: &str) -> Card {
-        let suit = card_as_str.chars().next();
-
-        let suit = match suit.expect("can't make a card from an empty string") {
-            'H' => Hearts,
-            'S' => Spades,
-            'D' => Diamonds,
-            'C' => Clubs,
-            _ => panic!("Not a valid suit"),
-        };
-
-        let value: u8 = card_as_str[1..].parse().expect("Not a valid number");
-        Card { suit, value }
+    impl Player for AlwaysFoldingPlayer {
+        fn do_play() -> PokerAction {
+            Fold
+        }
     }
 }
